78d244798ed14b23093f6a950b30b29e9cbc117c
ShivamSaini490 <126521640+ShivamSaini490@users.noreply.github.com>
2023-12-04 15:26:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            SomeMethod();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Caught in Main: " + ex.Message);
+        }
+
+        Console.WriteLine("Program continues...");
+    }
+
+    static void SomeMethod()
+    {
+        try
+        {
+            DivideByZero();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Caught in SomeMethod: " + ex.Message);
+            throw;
+        }
+    }
+
+    static void DivideByZero()
+    {
+        int numerator = 58;
+        int denominator = 0;
+
+        int result = numerator / denominator;
+    }
+}
